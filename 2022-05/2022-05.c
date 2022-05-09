35c341722e3b91ff43648eca6e26747a4afca70a
연어 <gwskid0426@naver.com>
2022-05-09 14:56:09 +0900

    Update 2022-05.c

---
+++ 2022-05/2022-05.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+// Pointer
