a368fbb50bc156a0c85fb2671d1b062bd28b32ca
연어 <gwskid0426@naver.com>
2022-11-07 04:19:38 +0900

    Upload Week10 Assignment

--- C Programming (2)/Assignment/Week10/BMP.c
+++ C Programming (2)/Assignment/Week10/BMP.c
@@ -1,24 +1,27 @@
+#define bmpFile "C:\\Users\\gwski\\Downloads\\resource\\BMP.bmp"
+#define nameFile "C:\\Users\\gwski\\Downloads\\resource\\name.bmp"
+
 #include <stdio.h>
 #include <windows.h>
-
-static HWND hWnd;
-static HINSTANCE hInst;
 
 int main(void) {
     HWND hWnd = GetConsoleWindow();
     HDC hDC = GetDC(hWnd);
     HDC hMemDC = CreateCompatibleDC(hDC);
+    SetStretchBltMode(hDC, COLORONCOLOR);
 
-	HBITMAP hBitmap = (HBITMAP)LoadImage(NULL, TEXT("C:\\Users\\gwski\\Downloads\\picture.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+    HBITMAP hBitmap = (HBITMAP)LoadImage(NULL, TEXT(bmpFile), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
     HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemDC, hBitmap);
+    BITMAP Bitmap; GetObject(hBitmap, sizeof(BITMAP), &Bitmap);
+    StretchBlt(hDC, 50, 50, 850, 400, hMemDC, 0, 0, Bitmap.bmWidth, Bitmap.bmHeight, SRCCOPY);
 
-    BITMAP Bitmap;
-    GetObject(hBitmap, sizeof(BITMAP), &Bitmap);
-
-    BitBlt(hDC, 50, 50, 50 + Bitmap.bmWidth, 50 + Bitmap.bmHeight, hMemDC, 0, 0, SRCCOPY);
+    HBITMAP nBitmap = (HBITMAP)LoadImage(NULL, TEXT(nameFile), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+    HBITMAP nOldBitmap = (HBITMAP)SelectObject(hMemDC, nBitmap);
+    GetObject(nBitmap, sizeof(BITMAP), &Bitmap);
+    StretchBlt(hDC, 400, 200, 150, 50, hMemDC, 0, 0, Bitmap.bmWidth, Bitmap.bmHeight, SRCCOPY);
 
     SelectObject(hMemDC, hOldBitmap);
-    DeleteObject(hBitmap); 
+    DeleteObject(hBitmap);
     DeleteDC(hMemDC);
     ReleaseDC(hWnd, hDC);
 
