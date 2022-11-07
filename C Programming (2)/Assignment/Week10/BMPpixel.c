2ec4df576e50d11a67b9513b0e4ce69ed21944d4
연어 <gwskid0426@naver.com>
2022-11-07 16:50:49 +0900

    Update BMPpixel.c

--- C Programming (2)/Assignment/Week10/BMPpixel.c
+++ C Programming (2)/Assignment/Week10/BMPpixel.c
@@ -13,12 +13,13 @@ static unsigned char* pBits;
 void editPixel(int x, int y, int c) {
     int width = ceil((double)Bitmap.bmWidth / (double)100);
     int height = ceil((double)Bitmap.bmHeight / (double)100);
+    int flag = abs( Bitmap.bmWidthBytes / (Bitmap.bmBitsPixel / 8) - Bitmap.bmWidth ) * 3;
 
-    for (int i = x * width; i < (x + 1) * width; i++) {
-        for (int j = y * height; j < (y + 1) * height; j++) {
-            pBits[0 + (j * 3 * Bitmap.bmWidth) + (i * 3) + (Bitmap.bmWidthBytes % 3) * j ] = c;
-            pBits[1 + (j * 3 * Bitmap.bmWidth) + (i * 3) + (Bitmap.bmWidthBytes % 3) * j ] = c;
-            pBits[2 + (j * 3 * Bitmap.bmWidth) + (i * 3) + (Bitmap.bmWidthBytes % 3) * j ] = c;
+    for (int j = y * height; j < (y + 1) * height; j++) {
+        for (int i = x * width; i < (x + 1) * width; i++) {
+            pBits[0 + (j * 3 * Bitmap.bmWidth) + (i * 3) + (Bitmap.bmWidthBytes % 3 + flag) * j] = c;
+            pBits[1 + (j * 3 * Bitmap.bmWidth) + (i * 3) + (Bitmap.bmWidthBytes % 3 + flag) * j] = c;
+            pBits[2 + (j * 3 * Bitmap.bmWidth) + (i * 3) + (Bitmap.bmWidthBytes % 3 + flag) * j] = c;
         }
     }
 }
@@ -28,12 +29,12 @@ void myName(void) {
 
     // '°í'
     // ¤Ç
-    for (i = 0; i < 9; i++) editPixel(i, 0, 255);
+    for (i = 0; i < 9; i++) editPixel(i, 0, 255); 
     for (i = 1; i < 8; i++) editPixel(i, 1, 0); editPixel(0, 1, 255); editPixel(8, 1, 255);
-    editPixel(0, 2, 255); editPixel(1, 2, 255); editPixel(2, 2, 255); editPixel(3, 2, 255); 
-    editPixel(4, 2, 0); 
+    editPixel(0, 2, 255); editPixel(1, 2, 255); editPixel(2, 2, 255); editPixel(3, 2, 255);
+    editPixel(4, 2, 0);
     editPixel(5, 2, 255); editPixel(6, 2, 255); editPixel(7, 2, 255); editPixel(8, 2, 255);
-	// ¤¡
+    // ¤¡
     editPixel(3, 3, 255); editPixel(4, 3, 0); editPixel(5, 3, 255);
     for (i = 3; i < 9; i++) editPixel(i, 4, 255);
     for (j = 5; j < 8; j++) { editPixel(6, j, 255); editPixel(7, j, 0); editPixel(8, j, 255); }
@@ -48,8 +49,8 @@ void myName(void) {
     editPixel(12, 1, 255); editPixel(13, 1, 0); editPixel(14, 1, 255);
     for (i = 9; i < 18; i++) editPixel(i, 2, 255); editPixel(12, 2, 255); editPixel(13, 2, 0); editPixel(14, 2, 255);
     for (i = 10; i < 17; i++) editPixel(i, 3, 0); editPixel(9, 3, 255); editPixel(17, 3, 255);
-    for (i = 9; i < 18; i++) editPixel(i, 4, 255);	
-	// ¤·
+    for (i = 9; i < 18; i++) editPixel(i, 4, 255);
+    // ¤·
     for (i = 10; i < 17; i++) { editPixel(i, 5, 255); editPixel(i, 6, 255); editPixel(i, 7, 255); editPixel(i, 8, 255); }
     editPixel(12, 5, 0); editPixel(13, 5, 0); editPixel(14, 5, 0);
     editPixel(11, 6, 0); editPixel(15, 6, 0);
@@ -70,9 +71,9 @@ void myName(void) {
     editPixel(20, 6, 0); editPixel(22, 6, 0);
     for (i = 19; i < 27; i++) for (j = 7; j < 9; j++) editPixel(i, j, 255);
     for (j = 5; j < 9; j++) { editPixel(25, j, 0); editPixel(26, j, 255); }
-    editPixel(20, 7, 0); editPixel(22, 7, 0); editPixel(23, 7, 0); editPixel(24, 7, 0); 
+    editPixel(20, 7, 0); editPixel(22, 7, 0); editPixel(23, 7, 0); editPixel(24, 7, 0);
     editPixel(21, 8, 0);
-    editPixel(20, 9, 255); editPixel(21, 9, 255); editPixel(22, 9, 255); 
+    editPixel(20, 9, 255); editPixel(21, 9, 255); editPixel(22, 9, 255);
     editPixel(24, 9, 255); editPixel(25, 9, 255); editPixel(26, 9, 255);
 }
 
@@ -86,6 +87,7 @@ int main(void) {
     HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemDC, hBitmap);
     GetObject(hBitmap, sizeof(BITMAP), &Bitmap);
     pBits = (unsigned char*)Bitmap.bmBits;
+
     myName();
 
     StretchBlt(hDC, 50, 50, 800, 400, hMemDC, 0, 0, Bitmap.bmWidth, Bitmap.bmHeight, SRCCOPY);
